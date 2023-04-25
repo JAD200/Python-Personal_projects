d7170f09c3748855d8a5e7068516be61ef7a25d2
JAD200 <juandipasquo@gmail.com>
2023-04-25 15:15:06 -0300

    Added click to the 'Next' button after sending the form data

--- order_charger/login_test.py
+++ order_charger/login_test.py
@@ -7,26 +7,30 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 #   Find_element
 from selenium.webdriver.common.by import By
-#   Sensible data
-from sensible import *
 #   Expected conditions and explicit waits
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 #   Current date time selector
 from current_datetime_selector import current_date
+# Medical benefits selection
+from medical_benefits_selector import medical_benefits
+#   Sensible data
+from sensible import *
 from time import sleep
 
+
+def click_next_button(driver, expected_xpath:str):
+    next_button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, expected_xpath)))
+    next_button.click()
 
 class LoginTest(unittest.TestCase):
 
     def setUp(self):
-        service = ChromeService(
-            executable_path=ChromeDriverManager().install())
+        service = ChromeService(executable_path=ChromeDriverManager().install())
         options = ChromeOptions()
         self.driver = webdriver.Chrome(service=service, options=options)
         driver = self.driver
-        options.add_experimental_option(
-            'excludeSwitches', ['enable-automation'])
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
         driver.implicitly_wait(6)
         driver.maximize_window()
         driver.get('https://bit.ly/3V2xSxz')
@@ -43,12 +47,6 @@ class LoginTest(unittest.TestCase):
         password_field.clear()
         password_field.send_keys(password_field_test)
         password_field.submit()
-        driver.implicitly_wait(10)
-        # Select TRÁMITES CONECTIVIDAD
-        # print('TRÁMITES CONECTIVIDAD')
-        # connectivity_button = driver.find_element(
-        # By.LINK_TEXT, "//a[@title='TRÁMITES CONECTIVIDAD']")
-        # connectivity_button.click()
         current_date(driver)
 
         # Attention place
@@ -56,11 +54,16 @@ class LoginTest(unittest.TestCase):
         driver.implicitly_wait(1)
         driver.find_element(
             By.XPATH, "//option[text()='25 DE MAYO 910']").click()
-        
+        # Affiliate number
         affiliate_number = driver.find_element(
             By.NAME, "validacionDelAfiliado")
         affiliate_number.send_keys(affiliate_number_test)
-        driver.implicitly_wait(4)
+
+        # Execute a JavaScript code to scroll to the bottom of the page
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # "NEXT" button
+        click_next_button(driver, "//button[normalize-space()='Siguiente']")
+        click_next_button(driver, "//button[normalize-space()='Siguiente']")
 
 
     def tearDown(self):
