765ec2b256bb2537ddc1cf3ebc86b12c67a358af
JAD200 <juandipasquo@gmail.com>
2023-04-25 13:32:32 -0300

    Added error handler and removed useless code

--- order_charger/current_datetime_selector.py
+++ order_charger/current_datetime_selector.py
@@ -1,23 +1,26 @@
-from selenium import webdriver
 #   find_element
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 # Current datetime
 from datetime import date
 
 
 def current_date(driver, date=date):
-    # Click in the calendar
-    driver.find_element(By.NAME, "fechaPrestacion").click()
-    # Identify div elements
-    dates_table = driver.find_elements(By.TAG_NAME, "div")
-    # Iterate over divs
-    today = date.today()
-    current_date = str(today.day)
-    print(current_date)
-    for i in dates_table:
-        # Verify required date then click
-        date = i.text
-        print(date)
-        if date == current_date:
-            i.click()
-            break
+    try:
+        # Click in the calendar
+        calendar_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "fechaPrestacion")))
+        calendar_element.click()
+        # Identify div elements
+        dates_divs = driver.find_elements(By.TAG_NAME, "div")
+        # Iterate over divs
+        today = date.today()
+        current_date = str(today.day)
+        for i in dates_divs:
+            # Verify required date then click
+            date = i.text
+            if date == current_date:
+                i.click()
+                break
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
